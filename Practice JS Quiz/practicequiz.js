568c0224f9df3d59b40954a2aa0d4ab58bfe4f3d
Mike Cook <mikecookdev@gmail.com>
2018-12-16 23:24:23 -0800

    Add files via upload

---
+++ Practice JS Quiz/practicequiz.js
@@ -0,0 +1,6 @@
+var answer = prompt('What was the first Pokemon ever created?');
+if ( answer.toUpperCase() === 'RHYDON' ) {
+	document.write("<p>Awesome! That's right!</p>");
+} else {
+	document.write ("<p> Sorry, that's wrong. Please try again.</p>");
+}
